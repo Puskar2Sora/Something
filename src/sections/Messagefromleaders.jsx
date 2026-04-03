4caf68f9463180b6b20da129faf54cd8601ca22f
Puskar Nath <puskarnath110@gmail.com>
2026-04-03 21:51:52 +0530

    fixed

--- src/sections/Messagefromleaders.jsx
+++ src/sections/Messagefromleaders.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import '../styles/MessageFromLeaders.css';
+import '../styles/Messagefromleaders.css';
 import Floatchar from '../components/Floatchar';
 
 /*
