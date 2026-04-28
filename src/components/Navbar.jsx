564afd4a611f7c3eb554355ce2737f970376171d
Puskar Nath <puskarnath110@gmail.com>
2026-04-28 15:18:38 +0530

    Changed the full navliks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,9 @@ const NAV_ICONS = {
   About:   'M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z',
   Events:  'M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z',
   Gallery: 'M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z',
+  Venue:'M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z M15 11a3 3 0 11-6 0 3 3 0 016 0z',
+  Groomers:'M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z',
+   FAQ: 'M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
 };
 
 const navLinks = [
@@ -14,6 +17,9 @@ const navLinks = [
   { name: 'About',   href: '#about' },
   { name: 'Events',  href: '#events' },
   { name: 'Gallery', href: '#gallery' },
+  {name : 'Groomers', href: '#winners'},
+  {name : 'Venue', href:'#venue'},
+  {name : 'FAQ', href:'#faq'}
 ];
 
 const NavIcon = ({ path }) => (
@@ -86,7 +92,7 @@ const Navbar = () => {
             <span className="dh-lithium">LITHIUM</span>
             <span className="dh-year">2K26</span>
           </div>
-          <div className="drawer-head-theme">✦ BEYOND THE VEIL ✦</div>
+          <div className="drawer-head-theme">✦ DREAMSCAPE ✦</div>
 
           
           {/* Close button */}
@@ -125,8 +131,6 @@ const Navbar = () => {
           ))}
         </ul>
 
-        {/* Ornate divider */}
-        <div className="drawer-divider" />
 
         {/* Footer */}
         <div className="drawer-foot">
